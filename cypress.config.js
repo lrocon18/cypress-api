fec6ab597b1d4a166d6e332cbb3111750abd884a
lrocon18 <lucas.piovezan8@gmail.com>
2023-07-02 00:28:01 -0300

    TESTE

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-      "reporter": "mochawesome",
-      "reporterOptions": {
-          "overwrite": false,
-          "html": false,
-          "json": true
-      }
-  },
+    "baseUrl": null,
+    "screenshotOnRunFailure": false,
+    "watchForFileChanges": false,
+    "viewportHeight": 720,
+    "viewportWidth": 1280,
+    "video": false,
+    "reporter": "mochawesome",
+    "reporterOptions": {
+      "overwrite": false,
+      "html": false,
+      "json": true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-
+  },
 });
